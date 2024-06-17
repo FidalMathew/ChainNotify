f31c45cc22b2bd28fcb0ddb1fc68823233e79aea
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-06-17 23:34:46 +0530

    dynamic initalized

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,6 +1,38 @@
+import Provider from "@/Provider";
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import {EthereumWalletConnectors} from "@dynamic-labs/ethereum";
+import {StarknetWalletConnectors} from "@dynamic-labs/starknet";
+import {DynamicContextProvider} from "@dynamic-labs/sdk-react-core";
+import type {AppProps} from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <Provider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <DynamicContextProvider
+        settings={{
+          environmentId: process.env.NEXT_PUBLIC_ENVIRONMENT_ID!,
+          initialAuthenticationMode: "connect-only",
+          walletConnectors: [
+            EthereumWalletConnectors,
+            StarknetWalletConnectors,
+          ],
+          bridgeChains: [
+            {
+              chain: "EVM",
+            },
+            {
+              chain: "STARK",
+            },
+          ],
+        }}
+      >
+        <Component {...pageProps} />
+      </DynamicContextProvider>
+    </Provider>
+  );
 }
